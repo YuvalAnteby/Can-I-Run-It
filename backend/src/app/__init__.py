c398696b629d7b32e79f3e8c95dde5a7a3c99d67
yuval <yuvala2001@gmail.com>
2025-05-25 10:25:42 +0300

    Removed files made by pycharm automatically (not needed)

--- backend/src/app/__init__.py
+++
@@ -1,9 +0,0 @@
-"""
-app module: Contains managing the general backend
-
-Modules:
-- main: initializes the FastAPI, connection to the DB amd config files
-- database: responsible for the connection to the mongoDB DB
-"""
-
-from .database import mongodb
