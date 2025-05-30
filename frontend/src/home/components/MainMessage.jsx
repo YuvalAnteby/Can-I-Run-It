2e8be947741f54c762a7eb46bacd49e8274c4751
yuval <yuvala2001@gmail.com>
2025-05-30 16:06:20 +0300

    Added animation to the image

--- frontend/src/home/components/MainMessage.jsx
+++ frontend/src/home/components/MainMessage.jsx
@@ -19,7 +19,19 @@ const MainMessage = () => {
                 <Typography variant="h3">Can my PC handle that?</Typography>
                 <Typography variant="h6">Instantly check game performance for your hardware</Typography>
             </Box>
-            <Box component="img" src="/home_img.png" sx={{width: 384, height: 384, marginLeft: 8}}/>
+            <Box component="img" src="/home_img.png"
+                 sx={{
+                     width: 384,
+                     height: 384,
+                     marginLeft: 8,
+                     // floating animation
+                     '@keyframes float': {
+                         '0%, 100%': {transform: 'translateY(0)'},
+                         '50%': {transform: 'translateY(-8px)'},
+                     },
+                     animation: 'float 6s ease-in-out infinite',
+                     willChange: 'transform',
+                 }}/>
 
         </Box>
     );
