15455fc3fda9e965a569288576828a9f63dab527
yuval <yuvala2001@gmail.com>
2025-10-18 13:45:09 +0300

    disable docs GUI in prod environment, added a variable in docker compose file for it

--- backend/src/app/main.py
+++ backend/src/app/main.py
@@ -10,11 +10,11 @@ from ..routes.health import router as health_router
 from ..routes.admin_hardware import router as admin_hardware_router
 from ..routes.admin_games import router as admin_games_router
 
-app = FastAPI(
-    title="Can I Run It API",
-    description="API for checking if your PC can run games",
-    version="0.1.0",
-    openapi_tags=[
+# Environment variable to distinguish between development and production
+ENV = os.environ.get("ENV", "dev")
+
+# Define route tags for documentation
+route_tags = [
         {"name": "CPUs", "description": "Endpoints for CPU data and info."},
         {"name": "GPUs", "description": "Endpoints for GPU data and info."},
         {"name": "Games", "description": "Endpoints for retrieving game information."},
@@ -22,8 +22,16 @@ app = FastAPI(
         {"name": "Admin - Games", "description": "Admin routes to manage gamed in MongoDB."},
         {"name": "Admin - Hardware", "description": "Admin routes to manage hardware in MongoDB."},
     ]
-)
 
+app = FastAPI(
+    title="Can I Run It API",
+    description="API for checking if your PC can run games",
+    version="0.1.0",
+    openapi_tags=route_tags,
+    docs_url=None if ENV != 'dev' else "/docs",
+    redoc_url=None if ENV != 'dev' else "/redoc",
+    openapi_url=None if ENV != 'dev' else "/openapi.json"
+)
 api_router = APIRouter(prefix="/api")
 
 # Include routers
@@ -37,15 +45,6 @@ api_router.include_router(health_router, prefix="/health", tags=["Health"])
 # disabled for now, will need to be switched to SQL DB for ML model
 #api_router.include_router(requirements_router, prefix="/req", tags=["Requirements"])
 app.include_router(api_router)
-
-@app.get("/")
-def read_root():
-    """
-    Welcome message to the root path.
-
-    :return:
-    """
-    return {"message": "Welcome to the Can You Run It Backend!"}
 
 # Add CORS middleware
 app.add_middleware(
