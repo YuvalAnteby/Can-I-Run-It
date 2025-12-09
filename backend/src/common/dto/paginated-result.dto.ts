a8faba2457421186b96cf00d0237b4cb9c1dd922
yuval <yuvala2001@gmail.com>
2025-12-09 22:34:08 +0200

    bugfix for swagger

--- backend/src/common/dto/paginated-result.dto.ts
+++ backend/src/common/dto/paginated-result.dto.ts
@@ -1,10 +1,20 @@
+import { ApiProperty } from "@nestjs/swagger";
+
 export class PaginationMeta {
+    @ApiProperty()
     total: number;
+    
+    @ApiProperty()
     page: number;
+
+    @ApiProperty()
     lastPage: number;
 }
 
 export class PaginatedResult<T> {
+    @ApiProperty({ isArray: true })
     data: T[];
+
+    @ApiProperty({ type: () => PaginationMeta })
     meta: PaginationMeta;
 }
