7076837476aea7d94f40ba0e468277750656c9f3
yuval <yuvala2001@gmail.com>
2026-01-19 14:29:16 +0200

    formatting

--- backend/src/cpu/cpu.module.ts
+++ backend/src/cpu/cpu.module.ts
@@ -1,7 +1,7 @@
 import { Module } from '@nestjs/common';
+
 import { DatabaseModule } from '../database/database.module';
 import { TypeOrmGpuRepository } from '../gpu/gpu.typeorm.repository';
-
 import { CpuController } from './cpu.controller';
 import { CpuService } from './cpu.service';
 import { ICpuRepositoryToken } from './icpu.repository';
